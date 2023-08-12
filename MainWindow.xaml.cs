dd3681773fccf7ecf16ecf1ec50e10be8cea290f
Christoph Herrmann <christoph.herrmann@theconfigteam.com>
2023-08-12 11:07:09 +1000

    Initial commi

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.Windows;
+
+namespace onevpnBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Uri mUrl;
+        private string mCookieName;
+
+        public MainWindow(Uri url, string cookieName)
+        {
+            InitializeComponent();
+
+            mUrl = url;
+            mCookieName = cookieName;
+
+            this.webView.NavigationCompleted += CheckCookies;
+            this.webView.Source = mUrl;
+        }
+
+        private async void CheckCookies(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (sender == null || sender is not WebView2)
+                return;
+
+            var webview = (WebView2)sender;
+
+            var cookies = await webview.CoreWebView2.CookieManager.GetCookiesAsync(mUrl.ToString());
+
+            foreach (var cookie in cookies)
+            {
+                if (!cookie.Name.Equals(mCookieName, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                Console.WriteLine(cookie.Name + "=" + cookie.Value);
+                this.Close();
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace onevpnBrowser
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    Console.WriteLine("Call with URL and cookie name");
+                    return;
+                }
+
+
+                var url = new Uri(args[0]);
+
+                if (string.IsNullOrWhiteSpace(args[1]))
+                {
+                    Console.WriteLine("No cookie name given");
+                    return;
+                }
+
+                RunApplication(url, args[1]);
+            }
+            catch (UriFormatException ex)
+            {
+                Console.WriteLine("URL " + args[0] + " incorrect");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("No URL given");
+            }
+        }
+
+        private static void RunApplication(Uri url, string cookieName)
+        {
+            MainWindow wnd = new MainWindow(url, cookieName);
+            var application = new System.Windows.Application();
+            application.Run(wnd);
+        }
+    }
+}
